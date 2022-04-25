8e405a150e47686f4c0affd6995a3fa8674e4dcc
Clayton Knapp <csjknapp@gmail.com>
2022-04-25 18:05:31 -0500

    tests in App.test written for header image and profile name

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,31 @@
+import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+import App from './App'
 
-test('Should render the header', () => {
+describe('Header Tests', () => {
+  it('Should render the header image', () => {
+    render(<App />)
 
+    // test for header image
+    screen.getByAltText('Alchemy Logo')
+
+    // screen.debug()
+  })
+
+  it('Should render the header profile name', async () => {
+    render(<App />)
+
+    // test for profile name
+    await screen.findByText('Vonta')
+
+    // screen.debug()
+  })
+
+  // it('Should render the header background color', async () => {
+  //   render(<App />)
+
+  //   // test for background color
+
+  //   // screen.debug()
+  // })
 })
